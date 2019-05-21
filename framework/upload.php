9a341cd6cead774d1d418ecfaa484de3e3b483ae
STAGIAIRE <cecilephilippe31@gmail.com>
2019-05-21 16:08:02 +0200

    framework upload: suppressions des espaces

--- framework/upload.php
+++ framework/upload.php
@@ -17,32 +17,22 @@ class Upload {
     }
 
     public function sauver($chemin) {
-
         if (!move_uploaded_file($this->cheminServeur, $chemin)) {
-
             $this->tabErreur[] = "Enregistrement du fichier impossible.";
         }
     }
 
     public static function maxFileSize($enOctets = true) {
-
         $kmg = ini_get('upload_max_filesize');
-
         if (!$enOctets) {
-
             return $kmg;
         }
 
         $kmg = str_ireplace('G', '*1024**3+', $kmg);
-
         $kmg = str_ireplace('M', '*1024**2+', $kmg);
-
         $kmg = str_ireplace('K', '*1024+', $kmg);
-
         $kmg .= '0';
-
         eval("\$octets = {$kmg};");
-
         return $octets;
     }
 
