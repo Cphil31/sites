360890055cef292a51eb7f66d9bd36c54f231d1e
STAGIAIRE <cecilephilippe31@gmail.com>
2019-05-21 14:35:44 +0200

    class update

---
+++ commerce_8/class/Upload.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Upload
+ *
+ * @author STAGIAIRE
+ */
+class Upload {
+
+    public $nomChamp;
+    public $tabExt = [];
+    public $tabMIME = [];
+    public $nomClient;
+    public $extension;
+    public $cheminServeur;
+    public $codeErreur;
+    public $octets;
+    public $typeMIME;
+    public $tabErreur = [];
+
+}
