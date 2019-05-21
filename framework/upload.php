0d4bd762e9764520f7fe6c6c521fb47b8eb10140
STAGIAIRE <cecilephilippe31@gmail.com>
2019-05-21 15:59:28 +0200

    framework upload

--- framework/upload.php
+++ framework/upload.php
@@ -1,20 +1,8 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Upload
- *
- * @author STAGIAIRE
- */
 class Upload {
 
     public $nomChamp;
-    public $tabExt = [];
     public $tabMIME = [];
     public $nomClient;
     public $extension;
@@ -24,4 +12,40 @@ class Upload {
     public $typeMIME;
     public $tabErreur = [];
 
+    public function __construct() {
+        
+    }
+
+    public function sauver($chemin) {
+
+        if (!move_uploaded_file($this->cheminServeur, $chemin)) {
+
+            $this->tabErreur[] = "Enregistrement du fichier impossible.";
+        }
+    }
+
+    public static function maxFileSize($enOctets = true) {
+
+        $kmg = ini_get('upload_max_filesize');
+
+        if (!$enOctets) {
+
+            return $kmg;
+        }
+
+        $kmg = str_ireplace('G', '*1024**3+', $kmg);
+
+        $kmg = str_ireplace('M', '*1024**2+', $kmg);
+
+        $kmg = str_ireplace('K', '*1024+', $kmg);
+
+        $kmg .= '0';
+
+        eval("\$octets = {$kmg};");
+
+        return $octets;
+    }
+
 }
+
+var_dump(Upload::maxFileSize());
