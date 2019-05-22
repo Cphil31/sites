0cabc5c60bfbacc67904d7c57292abb0fedacaaa
STAGIAIRE <cecilephilippe31@gmail.com>
2019-05-22 09:26:08 +0200

    commerce : etape 0

--- commerce_maison/index.php
+++ commerce_maison/index.php
@@ -1,62 +1,18 @@
-<?php
-require_once 'inc/cfg.php';
-//je fait ma requete 
-$req = "SELECT * FROM produit ORDER BY nom";
-$jeu = $pdo->query($req); /* methode PDO statement */
-$jeu->setFetchMode(PDO::FETCH_OBJ);
-$tab = $jeu->fetchAll(); /* methode PDO statement */
-//je fait ma requete 
-?>
 <!DOCTYPE html>
-
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title><?= TITRE ?></title>
-        <link href="css/commerce.css" rel="stylesheet">
-        <script type="text/javascript" src="js/index.js"></script>
+        <title></title>
+        <link href="../commerce/css/commerce.css" rel="stylesheet" type="text/css"/>
     </head>
-
     <body>
-        <header></header>
-        <div id="container">
-            <div class="categorie">
-                Produits
-            </div>
-         
-            <?php
-            foreach ($tab as $prod) {
-                /* file_exists(chemin du fichier) consigne: faire en sorte que si le fichier n'existe pas on as l'image d'interrgogation
-                 * déterminer une variable =0 
-                 * si la variable = 0 
-                 * ne pas modifier id produit */
-                $image = "../commerce/img/prod_{$prod->id_produit}_v.jpg";
-
-                if (file_exists($image)) {
-                    $image = "../commerce/img/prod_{$prod->id_produit}_v.jpg";
-                } else {
-                    $image = "../commerce/img/prod_0_v.jpg";
-                }
-                /*
-                 * Correction 
-                 * $id= file_exists("img/prod_{$prod->id_produit}_v.jpg")? $prod->id_produit :0;
-                 */
-                ?>
-
-                <div class="blocProduit" onclick="detail(<?= $prod->id_produit ?>)">
-                    <img src="<?= $image ?>"/> 
-                    <div class="nom"> <?= $prod->nom ?> </div>
-                </div>
-
-                <?php
-            }
-            ?>
-
-
-        </div>
-
-        <footer></footer>
-
-
+        <?php
+        // put your code here
+        ?>
     </body>
 </html>
